15b4346461f095e89eaa74a285f8fdb685ae0f8c
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2025-05-26 04:30:58 +0800

    Refactor code structure for improved readability and maintainability

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/users');
 const multer = require('multer');
 const users = require('../models/users');
+const fs = require('fs');
 
 // image upload configuration
 var storage = multer.diskStorage({
@@ -72,6 +73,38 @@ router.get('/edit/:id', async (req, res) => {
   } catch (err) {
     res.redirect('/');
   }
-})
+});
+
+// update user
+  router.post('/update/:id', upload, async (req, res) => {
+    let id = req.params.id;
+    let new_image = '';
+  
+    if (req.file) {
+      new_image = req.file.filename;
+      try {
+        fs.unlinkSync('./uploads/' + req.body.old_image);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      new_image = req.body.old_image;
+    }
+  
+    User.findByIdAndUpdate(id, {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      image: new_image  
+    }).then(() => {
+      req.session.message = {
+        type: 'success',
+        message: 'User updated successfully!'
+      };
+      res.redirect('/');
+    }).catch((err) => {
+      res.json({ message: err.message, type: 'danger' });
+    });
+  });
 
 module.exports = router;
